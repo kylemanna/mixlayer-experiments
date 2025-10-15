89958be3f0c2669bac5bc9b040b13557ec17e51f
Kyle Manna <kyle@kylemanna.com>
2025-10-14 20:00:21 -0700

    quickstart: More flake wrapping

--- quickstart/flake.nix
+++ quickstart/flake.nix
@@ -25,9 +25,42 @@
 
           shellHook = ''
             echo "Setting up development environment..."
+            export MODELSOCKET_API_KEY=$(${pkgs.libsecret}/bin/secret-tool lookup api mixlayer)
             npm install modelsocket
             echo "modelsocket installed successfully!"
           '';
+        };
+
+        packages.default = pkgs.buildNpmPackage {
+          pname = "mixlayer-quickstart";
+          version = "0.1.0";
+          src = ./.;
+
+          npmDepsHash = "sha256-nMR+AVWiO9ETxYdFa4tLNMVDPkQebJdTnmA9R8b0uQQ=";
+
+          dontNpmBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            cp -r node_modules index.mjs $out/bin/
+
+            runHook postInstall
+
+            cat > $out/bin/mixlayer-quickstart << EOF
+            #!/bin/sh
+            export MODELSOCKET_API_KEY=\$(${pkgs.libsecret}/bin/secret-tool lookup api mixlayer)
+            cd $out/bin
+            exec ${pkgs.nodejs}/bin/node index.mjs "\$@"
+            EOF
+            chmod +x $out/bin/mixlayer-quickstart
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mixlayer-quickstart";
         };
       }
     );
