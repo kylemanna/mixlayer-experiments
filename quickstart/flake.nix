e7d932a81c0a8f914524d080a502a064312e0d19
Kyle Manna <kyle@kylemanna.com>
2025-10-14 19:48:26 -0700

    flake: Add flake and friends

---
+++ quickstart/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Dev shell with Node.js and modelsocket";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+          ];
+
+          shellHook = ''
+            echo "Setting up development environment..."
+            npm install modelsocket
+            echo "modelsocket installed successfully!"
+          '';
+        };
+      }
+    );
+}
